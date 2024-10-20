b71e92b8023583a03df74b81dd9ccd07e796cf68
AnaisP974 <anaisperigny31@gmail.com>
2024-10-20 18:16:21 +0200

    redirection home page 'access denied'

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,7 +18,6 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        
        // return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
