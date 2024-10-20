e711f5c2c6e0d73acc24e13d237bd46dc077fc28
AnaisP974 <anaisperigny31@gmail.com>
2024-10-20 19:11:01 +0200

    Hash Pasword after updated User

--- src/Controller/ProfilUserController.php
+++ src/Controller/ProfilUserController.php
@@ -6,21 +6,30 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/profil')]
 final class ProfilUserController extends AbstractController
 {
     #[Route('/{id}/edit', name: 'app_profil_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer le mot de passe saisi
+            $password = $form->get('password')->getData();
+
+            // Si l'utilisateur a saisi un nouveau mot de passe, on le hache
+            if ($password) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $password);
+                $user->setPassword($hashedPassword);
+            }
             $entityManager->flush();
 
             return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
